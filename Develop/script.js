29c927418ac7fe8a0d7145d18123516ecbc83224
mlmcdan1 <77369368+mlmcdan1@users.noreply.github.com>
2022-06-14 20:03:24 -0500

    first commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,125 @@
 // Assignment code here
+var password=document.getElementById("password");
+var password = "";
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword(event){
+  event.preventDefault();
+
+//asks for the length of password
+  let lengthInputQuestion = window.prompt("What is the length of your string?");
+
+
+//empty array for options
+  let chooseOptions = [];
+
+// Questions that we're going to ask the users
+  let questionLoopArray = [
+    "Do you want Lower Case?", 
+    "Do you want upper case? ",
+    "Do you want numerics? ", 
+    "Do you want special characters?"
+  ];
+
+ // Loops through the array of questions for user
+  for (let i=0; i< questionLoopArray.length; ++i){
+    let userPrompt = window.prompt(questionLoopArray[i] + "Type yes or no");
+    
+  // if yes 
+    if (userPrompt.toLowerCase()== "yes"){
+        chooseOptions.push(i)
+      }
+      
+    }
+  
+    
+// calls generatePassword function
+  var password = generatePassword(Number(lengthInputQuestion), chooseOptions);
+
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var divHeader = document.querySelector(".card-header");
 
+  passwordText.value = password;
 }
+
+
+function generatePassword(length , array){
+
+  //array of lower case letters
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  //makes lower case letters all capitol 
+  const upperCaseLetters = lowerCaseLetters.toUpperCase();
+  //array of numbers 
+  const numbers = "0123456789";
+  //array of special characters
+  const special = ['@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ';',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  ']',
+  '~',
+  '-',
+  '_',
+  '.'].join("");
+
+  //empty string
+  let generateString = "";
+
+  //loop 
+  for (let i = 0; i<length; ++i)
+  {
+
+  //selects random number in range of the length
+  let randomNumberWithRangeOfArrayOfOptions = array[Math.floor(Math.random() * array.length)];
+  console.log(randomNumberWithRangeOfArrayOfOptions)
+
+
+  //random numbers picks one of the options 
+  switch (randomNumberWithRangeOfArrayOfOptions){
+    case 0:
+      generateString += lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)]
+    break;
+
+    case 1:
+      generateString += upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)]    
+      break
+
+    case 2:
+      generateString += numbers[Math.floor(Math.random() * numbers.length)]
+      break
+
+    case 3:
+      generateString += special[Math.floor(Math.random() * special.length)]
+      break
+    default:
+      console.log("error")
+   }
+  }
+  //returns the string
+  return  generateString
+}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Password Generator Starter Code
+In Challenge 3 I was able to apply what I've learned about loops, arrays, and functions in Javascript. 
+
+In the js file i've used a for loop and the math built in function to generate a random number, and a switch statement to select the characters for the passwords. 
+
+Screenshot of the page: ![PasswordGenerator](PswdGenerator.png)
+
+Link to deployed application: 
